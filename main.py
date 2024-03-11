d624b6f40766f410f4d6c43efc14272ce50b475a
Aziz Khasyi <azizkhasyi11@gmail.com>
2024-03-11 14:40:15 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+from tkinter import messagebox
+import pyfirmata
+from time import sleep
+
+# Functions
+# TODO: Add more features
+def LedON():
+    '''Turn on the LED on pin 13'''
+    board.digital[13].write(1)
+    
+def LedOFF():
+    '''Turn off the LED on pin 13'''
+    board.digital[13].write(0)
+    
+def LedBlink():
+    '''Blink the LED on pin 13 for a number of times given by the user. The default is 5 times.'''
+    count = 0 if blinkCount.get() == '' else int(blinkCount.get())
+    
+    if count == 0:
+        messagebox.showerror("Error", "Please enter a valid number")
+        return
+    elif count >= 30:
+        messagebox.showinfo("Warning", "The number is too high, it may damage the LED")
+        return
+    elif count > 10:
+        messagebox.askyesno("Warning", "The number is more than 10, it may take a long time. Do you want to continue?")
+        
+    for i in range(count):
+            board.digital[13].write(1)
+            sleep(0.5)
+            board.digital[13].write(0)
+            sleep(0.5)
+    
+board = pyfirmata.Arduino('COM8')
+
+win = tk.Tk()
+
+win.title("LED Control")
+win.minsize(400,60)
+
+label = tk.Label(win, text="Click to turn ON/OFF")
+label.grid(column=1, row=1)
+
+# Blink time
+blinkCount = tk.Entry(win, bd=2, width=8)
+blinkCount.grid(column=1, row=5)
+tk.Label(win, text="Blink time").grid(column=2, row=5)
+
+# Button
+ONbtn = tk.Button(win, bd=2, text="ON", command=LedON)
+ONbtn.grid(column=1, row=3)
+OFFbtn = tk.Button(win, bd=2, text="OFF", command=LedOFF)
+OFFbtn.grid(column=2, row=3)
+BLbtn = tk.Button(win, bd=2, text="Blink", command=LedBlink)
+BLbtn.grid(column=1, row=6)
+BLbtn = tk.Button(win, bd=2, text="Stop Blink", command=LedBlink)
+BLbtn.grid(column=2, row=6)
+
+win.mainloop()
